83a92a76945c19884a5d510e1e151145b5f08c7f
Lydia-09 <pasky0901@gmail.com>
2021-09-28 19:40:21 +0800

    feat: add create & show page

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// 載入 express, mongoose 並建構應用程式伺服器
+// 載入 express, mongoose, body-parser 並建構應用程式伺服器
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Restaurant = require('./models/restaurant')
-const restaurant = require('./models/restaurant')
 const app = express()
 const port = 3000
 
@@ -24,6 +24,8 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // setting static files
 app.use(express.static('public'))
 
@@ -33,6 +35,26 @@ app.get('/', (req, res) => {
   .lean()
   .then(restaurants => res.render('index', { restaurants }))
   .catch(error => console.log(error))
+})
+
+// 路由：新增餐廳  
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+// 路由：瀏覽特定餐廳
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants', (req, res) => {
+  return Restaurant.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 // start and listen on Express server
