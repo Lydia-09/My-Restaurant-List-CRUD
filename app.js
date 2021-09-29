978d8240ef61e171bf958c593aed0bed48bdd223
Lydia-09 <pasky0901@gmail.com>
2021-09-29 19:30:13 +0800

    feat: add delete function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const Restaurant = require('./models/restaurant')
+const restaurant = require('./models/restaurant')
 const app = express()
 const port = 3000
 
@@ -64,6 +65,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
   const id = req.params.id
   return Restaurant.findByIdAndUpdate(id, { $set: req.body })
     .then(()=> res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+//delete
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
